7937711029fde79e51f172b7a4cc51677942965c
Paulo José dos Santos <paulo-santos_07@hotmail.com>
2024-08-07 21:26:05 -0300

    detectando qual a operação

--- scripts.js
+++ scripts.js
@@ -6,9 +6,32 @@ function appendNumber(number) {
     if(previousNumber.includes(".") && number == ".") return
     previousNumber += number
     console.log(previousNumber);
+
+    updateScreen()
+}
+
+function updateScreen(){
+    if (operation == ""){
+        $screenDisplay.innerHTML = previousNumber
+    } else {
+        $screenDisplay.innerHTML = `${previousNumber} ${operation}`
+    }
+}
+
+function chooseOperation(operator) {
+    operation = operator
+
+    updateScreen()
 }
 
 const $numberButtons = document.querySelectorAll("[data-number]")
-$numberButtons.forEach(button => button.addEventListener("click", () => {
-    appendNumber(button,dataset.number)
-}))
+$numberButtons.forEach(button => button.addEventListener("click", () => 
+    appendNumber(button.dataset.number)
+))
+
+const $screenDisplay = document.querySelector(".screen-display")
+
+const $operatorButtons = document.querySelectorAll("[data-operator]")
+$operatorButtons.forEach(button => button.addEventListener("click", () => 
+    chooseOperation(button.dataset.operator)
+))
